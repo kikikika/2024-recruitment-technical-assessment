5e047fde79a6417339870becf4ad0420a4dd4802
Krista <krista.chen@student.unsw.edu.au>
2024-02-25 23:37:34 +1100

    changed colour of sort box

--- frontend/my-app/src/header.js
+++ frontend/my-app/src/header.js
@@ -66,7 +66,7 @@ const Header = () => {
                 }}>
                 <button class="sort" type="button">
                     Sort by
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="icon">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="icon">
                         <path d="M12.53 16.28a.75.75 0 01-1.06 0l-7.5-7.5a.75.75 0 011.06-1.06L12 14.69l6.97-6.97a.75.75 0 111.06 1.06l-7.5 7.5z"></path>
                     </svg>
                 </button>
